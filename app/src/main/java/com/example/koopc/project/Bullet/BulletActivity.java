165f95c3c4dc5678ccc571f248b30728160e939e
koo <rnwndrms1@naver.com>
2017-12-10 20:56:26 +0900

    게시판 수정

--- app/src/main/java/com/example/koopc/project/Bullet/BulletActivity.java
+++ app/src/main/java/com/example/koopc/project/Bullet/BulletActivity.java
@@ -2,10 +2,11 @@ package com.example.koopc.project.Bullet;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -20,6 +21,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class BulletActivity extends AppCompatActivity {
+    private String searchText;
     private String buildingName;
     private BulletAdapter bulletAdapter;
     private ListView listView;
@@ -37,30 +39,6 @@ public class BulletActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.Bullet_view);
         listView.setAdapter(bulletAdapter);
         setBulletData();
-
-
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        // 게시판 클릭했을 때 활성화
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                BulletDTO bulletItem = (BulletDTO) bulletAdapter.getItem(position);
-                String name = bulletItem.getMessageTitle();
-                String content = bulletItem.getMessageContent();
-
-                Intent intent = new Intent(getApplicationContext(), BulletMsgActivity.class);
-                intent.putExtra("name", name);
-                intent.putExtra("content", content);
-                intent.putExtra("type", "watchMode");
-                startActivity(intent);
-            }
-        });
     }
 
     // 게시판 데이터 세팅하기
@@ -69,24 +47,53 @@ public class BulletActivity extends AppCompatActivity {
         bulletNameRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-//                int resId = getResources().getIdentifier("ic_launcher", "mipmap", getPackageName()); // 디폴트 그림 받기
 
                 bulletAdapter.clear();
 
-                for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                    if (ds.child("buildingName").getValue().toString().equals(buildingName)) {
-                        BulletDTO dto = new BulletDTO(); // DTO 는 item 값. item 에 해당 데이터 3개 세팅할것.
-                        dto.setMessageTitle(ds.child("messageTitle").getValue().toString());
-                        dto.setMessageContent(ds.child("messageContent").getValue().toString());
-                        dto.setMessageDate(ds.child("messageDate").getValue().toString());
-                        dto.setMessageType(ds.child("messageType").getValue().toString());
-                        dto.setBuildingName(ds.child("buildingName").getValue().toString());
+                if(searchText == null) {
+                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                        if (ds.child("buildingName").getValue().toString().equals(buildingName)) {
+                            BulletDTO dto = new BulletDTO(); // DTO 는 item 값. item 에 해당 데이터 3개 세팅할것.
+                            dto.setMessageTitle(ds.child("messageTitle").getValue().toString());
+                            dto.setMessageContent(ds.child("messageContent").getValue().toString());
+                            dto.setMessageDate(ds.child("messageDate").getValue().toString());
+                            dto.setMessageType(ds.child("messageType").getValue().toString());
+                            dto.setBuildingName(ds.child("buildingName").getValue().toString());
 
-                        bulletAdapter.addItem(dto); // 세팅한 데이터를 토대로 어댑터에 추가하기.
-//                        bulletAdapter.notifyDataSetChanged();
-                        listView.setAdapter(bulletAdapter);
+                            bulletAdapter.addItem(dto); // 세팅한 데이터를 토대로 어댑터에 추가하기.
+                            listView.setAdapter(bulletAdapter);
+                        }
+                    }
+                } else {
+                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                        if (ds.child("messageTitle").getValue().toString().equals(searchText) && ds.child("buildingName").getValue().toString().equals(buildingName)) {
+                            BulletDTO dto = new BulletDTO(); // DTO 는 item 값. item 에 해당 데이터 3개 세팅할것.
+                            dto.setMessageTitle(ds.child("messageTitle").getValue().toString());
+                            dto.setMessageContent(ds.child("messageContent").getValue().toString());
+                            dto.setMessageDate(ds.child("messageDate").getValue().toString());
+                            dto.setMessageType(ds.child("messageType").getValue().toString());
+                            dto.setBuildingName(ds.child("buildingName").getValue().toString());
+
+                            bulletAdapter.addItem(dto); // 세팅한 데이터를 토대로 어댑터에 추가하기.
+                            listView.setAdapter(bulletAdapter);
+                        }
                     }
                 }
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        BulletDTO bulletItem = (BulletDTO) bulletAdapter.getItem(position);
+                        String name = bulletItem.getMessageTitle();
+                        String content = bulletItem.getMessageContent();
+
+                        Intent intent = new Intent(getApplicationContext(), BulletMsgActivity.class);
+                        intent.putExtra("name", name);
+                        intent.putExtra("content", content);
+                        intent.putExtra("type", "watchMode");
+                        startActivity(intent);
+                    }
+                });
             }
 
             @Override
@@ -122,19 +129,21 @@ public class BulletActivity extends AppCompatActivity {
                 BulletDTO dto = new BulletDTO(item1, item2, item3, item4, item5);
                 bulletNameRef.push().setValue(dto);
                 Toast.makeText(getApplicationContext(), "성공적으로 추가됨.", Toast.LENGTH_LONG).show();
+
+                searchText = null;
+                setBulletData();
             } else if (resultCode == Activity.RESULT_CANCELED) {
-                Toast.makeText(getApplicationContext(), "취소되었음.", Toast.LENGTH_LONG).show();;
+                Toast.makeText(getApplicationContext(), "취소되었음.", Toast.LENGTH_LONG).show();
+
+                searchText = null;
+                setBulletData();
             }
         }
     }
 
-    public void bulletNextClick(View view) {
-        // 다음 10개 보여주기
+    public void button_searchBullet(View view) {
+        EditText editText = (EditText) findViewById(R.id.edit_searchBullet);
+        searchText = editText.getText().toString();
+        setBulletData();
     }
-
-    public void bulletPrevClick(View view) {
-        // 이전 10개 보여주기
-    }
-
-    // 페이지 세팅 할것.
 }
